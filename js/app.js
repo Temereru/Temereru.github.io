de62dbc52f2151c9560ee4cf92747a53a7227ada
Tomer David <younrg@gmail.com>
2016-05-25 09:00:06 +0200

    added basic route testing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('main', ['ui.router']);
+var app = angular.module('main', ['ui.router'])
+  .config(function($stateProvider, $urlRouterProvider){
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'html/home.html'
+      });
+    $stateProvider
+      .state('projects', {
+        url: '/projects',
+        templateUrl: 'html/projects.html'
+      })
+  })
